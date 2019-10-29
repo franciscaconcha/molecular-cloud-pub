063bca71b77a30039d716c62540ce2e6d31c74b8
Francisca <fran.concha.r@gmail.com>
2019-10-29 15:39:52 +0100

    Fixed run.sh script

--- convergence_tests/run.sh
+++ convergence_tests/run.sh
@@ -6,7 +6,7 @@
 Nsph=(4000 8000 16000 32000)
 Nruns=(1 2 3 4 5 6 7 8)
 
-logfile="results/log_M$1MSun""_R$2pc.txt"
+logfile="results/5Myr/log_M$1MSun""_R$2pc.txt"
 echo "" > $logfile
 
 for Ns in ${Nsph[@]}
@@ -16,12 +16,12 @@ do
     do
         if [ $(expr $Ns \* $r) -le 32000 ]
         then
-            filepath="results/M$1MSun""_R$2pc_N$Ns""/$r"
+            filepath="results/5Myr/M$1MSun""_R$2pc_N$Ns""/$r"
             start=`date "+%Y-%m-%d %H:%M:%S"`
             start_s=`date "+%s"`
             echo $filepath >> $logfile
             echo "START: $start" >> $logfile
-            ../../amuse/amuse.sh cloud_collapse.py --Mcloud $1 --Rcloud $2 --Ncloud $Ns -s $filepath
+            ../../amuse/amuse.sh cloud_collapse.py --Mcloud $1 --Rcloud $2 --Ncloud $Ns -s $filepath --tend 5
             end=`date "+%Y-%m-%d %H:%M:%S"`
             end_s=`date "+%s"`
             echo "END: $end" >> $logfile
