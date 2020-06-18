ecdd0a328233d80044f01531f15af28c863afde6
Francisca <fran.concha.r@gmail.com>
2020-06-18 12:59:31 +0100

    Added proper criteria for star forming after sinks have stopped accreting

--- src/molecular_cloud_collapse_with_stars.py
+++ src/molecular_cloud_collapse_with_stars.py
@@ -206,7 +206,10 @@ def run_molecular_cloud(gas_particles, sink_particles, SFE, method, tstart, tend
 
             elif sink.mass < IMF_masses[current_mass] and sink.form_star:
                 print "Sink is not massive enough to form this star."
-                if not sink_formation:
+                sinks_masses = [s.mass for s in local_sinks]
+
+                # No sink has enough mass to form this star
+                if not sink_formation and all(i >= IMF_masses[current_mass] for i in sinks_masses):
                     # If the sinks are not accreting anymore, I keep moving through the IMF
                     # so that we don't get stuck trying to form a massive star with low mass sinks
                     current_mass += 1
