6299f557b53133ac8261cb1d08b1067227d60114
Francisca <fran.concha.r@gmail.com>
2020-11-03 14:45:50 +0100

    Fixing some gravhydro crashing issues

--- src/molecular_cloud_collapse_with_stars.py
+++ src/molecular_cloud_collapse_with_stars.py
@@ -213,7 +213,7 @@ def run_molecular_cloud(gas_particles, sink_particles, SFE, tstart, tend, dt_dia
                     gravhydro = Bridge()
                     gravhydro.add_system(gravity, (hydro.code,))
                     gravhydro.add_system(hydro.code, (gravity,))
-                    gravhydro.timestep = 0.1 * dt
+                    gravhydro.timestep = 0.5 * dt
                 else:
                     gravity.particles.add_particles(stars_from_sink)
                     gravity_to_framework.copy()
