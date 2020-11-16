470c463f65a5a2cd1b787381885aadb45c19659f
Francisca <fran.concha.r@gmail.com>
2020-11-16 17:15:03 +0100

    Added some run parameters to legends

--- plots/legends.py
+++ plots/legends.py
@@ -16,12 +16,29 @@ labels = {'R01': 'R = 0.1 pc',
           'N1E4R25': 'R = 2.5 pc',
           'N1E4R5': 'R = 5 pc'}
 
+# Number of stars created in each run
 Ns = {0: 6289,
       1: 6315,
       2: 6162,
       3: 6283,
       4: 5691,
       5: 6137}
+
+# t_end for star formation
+t_end = {0: 4.246,
+         1: 4.288,
+         2: 10.006,
+         3: 5.928,
+         4: 8.870,
+         5: 7.735}
+
+# t_ini for evolution without star formation
+t_ini = {0: 4.250,
+         1: 4.290,
+         2: 10.010,
+         3: 5.930,
+         4: 8.875,
+         5: 7.740}
 
 cmap = matplotlib.colors.ListedColormap(seaborn.color_palette('cubehelix', 13))
 cmap_colors = []
