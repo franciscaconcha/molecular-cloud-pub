df743ab3fef6e507f9ba4c1468bbb04490c6e858
Francisca <fran.concha.r@gmail.com>
2020-11-20 17:15:31 +0100

    fixed small bug in fractal dimesion for plummer sphere

--- plots/fractal_dimension.py
+++ plots/fractal_dimension.py
@@ -80,7 +80,7 @@ def fd_vs_time(open_path, nruns, save, save_path):
                   c=runcolors[n],
                   )
 
-    path = '{0}/plummer6k/0/'.format(open_path)
+    path = '{0}/plummer6k/'.format(open_path)
     print path
     files = os.listdir(path)  # = '{0}/M{1}MSun_R{2}pc_N{3}/{4}/'
     files = [x for x in files if '.hdf5' in x]
