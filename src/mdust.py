d729298343ccfc63eead2f1366bf0fd2f477c0cd
Francisca <fran.concha.r@gmail.com>
2020-11-23 18:12:48 +0100

    fixing small bug in mdust for plot

--- src/mdust.py
+++ src/mdust.py
@@ -68,7 +68,7 @@ def main(open_path, N, save_path, t_end, save, nruns):
 	      3: 6283,
 	      4: 5691,
 	      5: 6137}
-	sf_end_indices = [85, 86, 224, 127, 196, 171]
+	sf_end_indices = [85, 86, 224, 128, 196, 171]
 
 	for n in range(4, nruns):
 		N = Ns[n]
@@ -81,8 +81,8 @@ def main(open_path, N, save_path, t_end, save, nruns):
 		#print files
 		i = sf_end_indices[n]
 
-		prev_stars = read_set_from_file(path + files[i], 'hdf5', close_file=True)
-		t = float(files[i].split('t')[1].split('.hdf5')[0])
+		prev_stars = read_set_from_file(path + files[0], 'hdf5', close_file=True)
+		t = float(files[0].split('t')[1].split('.hdf5')[0])
 		prev_dust_mass = prev_stars.disk_dust_mass
 		prev_gas_mass = prev_stars.disk_gas_mass
 
@@ -93,12 +93,14 @@ def main(open_path, N, save_path, t_end, save, nruns):
 		                                                            t),
 		                  'hdf5')
 
-		for f in files[i+1:]:
+		for j in range(len(files[1:])):
+			f = files[j]
 			stars = read_set_from_file(path + f, 'hdf5', close_file=True)
 			t = float(f.split('t')[1].split('.hdf5')[0])
 
-			stars.disk_dust_mass = prev_dust_mass
-			stars.disk_gas_mass = prev_gas_mass
+			if j > i:
+				stars.disk_dust_mass = prev_dust_mass
+				stars.disk_gas_mass = prev_gas_mass
 
 			for s in stars[stars.disked]:
 				s.dust_mass = dust_mass(s, t)
