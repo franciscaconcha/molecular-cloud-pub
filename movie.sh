0d1cff71fca7b51e15b4bbc1eed0461e87773781
Francisca <fran.concha.r@gmail.com>
2021-01-13 11:47:53 +0000

    Cleaning up public repository

--- movie.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $1: framerate, 5 or less looks good
-# $2: path for files
-# $3: output name (including extension)
-
-ffmpeg -framerate $1 -i $2/%04d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p $2/$3
